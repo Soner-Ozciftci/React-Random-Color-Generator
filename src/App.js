5f7cd47eff08adbbb8bd97e48d5d851f4159b689
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-04 14:04:29 +0200

    add button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [hex, setHex] = useState("#fffff");
 
-const [ hex,setHex ] = useState("#fffff");
-
-function randomHex(){
-   const randomhex = '#' + Math.floor(Math.random() * 16777721 ).toString(16)
-}
+  function randomHex() {
+    const randomhex = "#" + Math.floor(Math.random() * 16777721).toString(16);
+    setHex(randomHex);
+  }
 
   return (
     <div className="App">
-
+      <h1>{hex}</h1>
+      <button>Click for New Background</button>
     </div>
   );
 }
